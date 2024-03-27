f48d07f0fb439adebf9740ede0d162148b232d46
phtuongvy <Tuong-Vy.Pham-Huynh@etu.univ-smb.fr>
2024-03-27 10:14:31 +0100

    push some crypto

--- SAE_API/Controllers/JWT/LoginController.cs
+++ SAE_API/Controllers/JWT/LoginController.cs
@@ -48,31 +48,47 @@ namespace SAE_API.Controllers.JWT
         {
             var comptes = await dataRepository.GetAllAsync();
 
-            //var hashedPassword = ComputeSha256Hash(user.Password.ToString());
+            var hashedPasswordUserInput = ComputeSha256Hash(user.Password);
 
             return comptes.Value.FirstOrDefault(x => x.Email.Trim().ToUpper() == user.Email.Trim().ToUpper() &&
-            x.Password == user.Password);
+            ComputeSha256Hash(x.Password) == hashedPasswordUserInput);
         }
         private string GenerateJwtToken(CompteClient userInfo)
         {
-            var securityKey = new
-           SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
             var claims = new[]
             {
-                 new Claim(JwtRegisteredClaimNames.Sub, userInfo.Email),
-                 new Claim("Email", userInfo.Email.ToString()),
+                 new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                 new Claim("email", userInfo.Email.ToString()),
                  new Claim("role",userInfo.ClientRole),
                  new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
              };
             var token = new JwtSecurityToken(
-             issuer: _config["Jwt:Issuer"],
-             audience: _config["Jwt:Audience"],
-             claims: claims,
-             expires: DateTime.Now.AddMinutes(30),
-             signingCredentials: credentials
+                 issuer: _config["Jwt:Issuer"],
+                 audience: _config["Jwt:Audience"],
+                 claims: claims,
+                 expires: DateTime.Now.AddMinutes(30),
+                 signingCredentials: credentials
              );
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        // to encrypt password data
+        private string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
         }
 
 
