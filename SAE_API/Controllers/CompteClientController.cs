b9211cbe3f620f3cac7ad3df31cc2b149725e972
phtuongvy <Tuong-Vy.Pham-Huynh@etu.univ-smb.fr>
2024-03-27 21:00:01 +0100

    changes

--- SAE_API/Controllers/CompteClientController.cs
+++ SAE_API/Controllers/CompteClientController.cs
@@ -124,11 +124,11 @@ namespace SAE_API.Controllers
         }
 
         // to encrypt password data
-        private string ComputeSha256Hash(string rawData)
+        private string ComputeSha256Hash(string password)
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
 
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
--- SAE_API/Controllers/JWT/LoginController.cs
+++ SAE_API/Controllers/JWT/LoginController.cs
@@ -75,11 +75,11 @@ namespace SAE_API.Controllers.JWT
         }
 
         // to encrypt password data
-        private string ComputeSha256Hash(string rawData)
+        private string ComputeSha256Hash(string password)
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
 
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
