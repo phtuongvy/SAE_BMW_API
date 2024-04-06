2560ef0a91fd5e07086518b2714d6d149bb27862
Shiro1234orihS <96548862+Shiro1234orihS@users.noreply.github.com>
2024-04-06 23:29:14 +0200

    j'ai fais les commentaire et les region pour le Achoisi Controller

--- SAE_APITests/Controllers/AChoisiControllerTests.cs
+++ SAE_APITests/Controllers/AChoisiControllerTests.cs
@@ -11,106 +11,133 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+/// <summary>
+/// Namespace contenant les tests pour le contrôleur AChoisiController.
+/// </summary>
 
 namespace SAE_API.Controllers.Tests
 {
+    /// <summary>
+    /// Classe de test pour AChoisiController. Contient des tests unitaires pour tester le comportement du contrôleur AChoisi.
+    /// </summary>
+    /// 
     [TestClass()]
     public class AChoisiControllerTests
     {
-       
+        #region Private Fields
+        // Déclaration des variables nécessaires pour les tests
         private AChoisiController controller;
         private BMWDBContext context;
         private IDataRepository<AChoisi> dataRepository;
+        #endregion
 
+        #region Test Initialization
 
+        /// <summary>
+        /// Méthode d'initialisation exécutée avant chaque test pour configurer l'environnement de test.
+        /// Initialise le contexte de base de données, le référentiel de données et le contrôleur AChoisi.
+        /// </summary>
         [TestInitialize]
         public void Init()
         {
+            // Configuration de la base de données pour les tests avec DbContextOptionsBuilder
             var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server = 51.83.36.122; port = 5432; Database = sa25; uid = sa25; password = 1G1Nxb; SearchPath = bmw");
             context = new BMWDBContext(builder.Options);
+            // Création du gestionnaire de données et du contrôleur à tester
             dataRepository = new AChoisiManager(context);
             controller = new AChoisiController(dataRepository);
         }
+        #endregion
 
-        /// <summary>   
-        /// Test Contrôleur 
+        #region Test Controller
+        /// <summary>
+        /// Teste l'instanciation du contrôleur AChoisiController pour s'assurer qu'elle n'est pas null.
         /// </summary>
         [TestMethod()]
         public void AChoisiControllerTest()
         {
-            // Arrange
+            // Arrange : préparation des données attendues
             var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server = 51.83.36.122; port = 5432; Database = sa25; uid = sa25; password = 1G1Nxb; SearchPath = bmw");
             context = new BMWDBContext(builder.Options);
             dataRepository = new AChoisiManager(context);
 
-            // Act
+            // Act : appel de la méthode à tester
             var option = new AChoisiController(dataRepository);
 
-            // Assert
+            // Assert : vérification que les données obtenues correspondent aux données attendues : vérification que les données obtenues correspondent aux données attendues
             Assert.IsNotNull(option, "L'instance de MaClasse ne devrait pas être null.");
 
         }
+        #endregion
 
+        #region Test GetAChoisiTest
 
         /// <summary>
-        /// Test GetCarteBancaireTest 
+        /// Teste la méthode GetAChoisis pour vérifier qu'elle retourne la liste correcte des éléments AChoisi.
         /// </summary>
         [TestMethod()]
         public void GetAChoisiTest()
         {
-            // Arrange
+            // Arrange : préparation des données attendues : préparation des données attendues
             List<AChoisi> expected = context.Achoisis.ToList();
-            // Act
+            // Act : appel de la méthode à tester 
             var res = controller.GetAChoisis().Result;
-            // Assert
+            // Assert : vérification que les données obtenues correspondent aux données attendues 
             CollectionAssert.AreEqual(expected, res.Value.ToList(), "Les listes ne sont pas identiques");
         }
+        #endregion
+
+        #region Test GetAChoisiByIdTest
 
         /// <summary>
-        /// Test GetCarteBancaireById 
+        /// Teste la méthode GetAChoisiById pour vérifier qu'elle retourne l'élément correct basé sur l'ID fourni.
         /// </summary>
-
         [TestMethod()]
         public void GetAChoisiByIdTest()
         {
-            // Arrange
+            // Arrange : préparation des données attendues
             AChoisi expected = context.Achoisis.Find(1, 1);
-            // Act
+            // Act : appel de la méthode à tester
             var res = controller.GetAChoisiById(expected.IDPack, expected.IDConfigurationMoto).Result;
-            // Assert
+            // Assert : vérification que les données obtenues correspondent aux données attendues
             Assert.AreEqual(expected, res.Value);
         }
 
+        /// <summary>
+        /// Teste la méthode GetAChoisiById en utilisant un mock pour le référentiel de données.
+        /// Permet de tester le contrôleur de manière isolée.
+        /// </summary>
         [TestMethod]
         public void GetAChoisiByIdTest_AvecMoq()
         {
-            // Arrange
-
+            // Arrange : préparation des données attendues
             var mockRepository = new Mock<IDataRepository<AChoisi>>();
-
-
-
 
             AChoisi option = new AChoisi
             {
                 IDPack = 1,
                 IDConfigurationMoto = 1,
             };
-            // Act
+
+            // Act : appel de la méthode à tester
             mockRepository.Setup(x => x.GetByIdAsync(option.IDPack, option.IDConfigurationMoto).Result).Returns(option);
             var userController = new AChoisiController(mockRepository.Object);
-
             var actionResult = userController.GetAChoisiById(option.IDPack, option.IDConfigurationMoto).Result;
-            // Assert
+
+            // Assert : vérification que les données obtenues correspondent aux données attendues
             Assert.IsNotNull(actionResult);
             Assert.IsNotNull(actionResult.Value);
             Assert.AreEqual(option, actionResult.Value as AChoisi);
         }
+        #endregion
 
+        #region Test PutAChoisiTestAsync
         /// <summary>
-        /// Test PutCarteBancaireTest 
+        /// Teste la méthode PutAChoisi pour vérifier que la mise à jour d'un élément fonctionne correctement.
         /// </summary>
-        [TestMethod()]
+
         public async Task PutAChoisiTestAsync()
         {
             //Arrange
@@ -127,10 +154,10 @@ namespace SAE_API.Controllers.Tests
             };
 
 
-            // Act
+            // Act : appel de la méthode à tester
             var res = await controller.PutAChoisi(optionAtester.IDPack, optionAtester.IDConfigurationMoto, optionUptade);
 
-            // Arrange
+            // Arrange : préparation des données attendues
             var nouvelleoption = controller.GetAChoisiById(optionUptade.IDPack, optionUptade.IDConfigurationMoto).Result;
             Assert.AreEqual(optionUptade, res);
 
@@ -138,11 +165,16 @@ namespace SAE_API.Controllers.Tests
             await context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Teste la méthode PutAChoisi en utilisant un mock pour simuler le référentiel de données.
+        /// Permet de vérifier le comportement du contrôleur lors de la mise à jour d'un élément.
+        /// </summary>
+
         [TestMethod]
         public void PutAChoisiTestAvecMoq()
         {
 
-            // Arrange
+            // Arrange : préparation des données attendues
             AChoisi optionToUpdate = new AChoisi
             {
                 IDPack = 40,
@@ -161,33 +193,34 @@ namespace SAE_API.Controllers.Tests
 
             var controller = new AChoisiController(mockRepository.Object);
 
-            // Act
+            // Act : appel de la méthode à tester
             var result = controller.PutAChoisi(40, 5, updatedOption).Result;
 
-            // Assert
+            // Assert : vérification que les données obtenues correspondent aux données attendues
             Assert.IsInstanceOfType(result, typeof(ActionResult<AChoisi>), "La réponse n'est pas du type attendu AChoisi");
         }
+        #endregion
+
+        #region Test PostAChoisiTestAsync
 
         /// <summary>
-        /// Test PostUtilisateur 
+        /// Teste la méthode PostAChoisi pour vérifier que l'ajout d'un nouvel élément fonctionne correctement.
         /// </summary>
-        /// 
+        
         [TestMethod()]
         public async Task PostAChoisiTestAsync()
         {
-            // Arrange
-
+            // Arrange : préparation des données attendues
             AChoisi option = new AChoisi
             {
                 IDPack = 1,
                 IDConfigurationMoto = 7,
             };
 
-            // Act
+            // Act : appel de la méthode à tester
             var result = controller.PostAChoisi(option).Result; // .Result pour appeler la méthode async de manière synchrone, afin d'attendre l’ajout
 
-            // Assert
-            // On récupère l'utilisateur créé directement dans la BD grace à son mail unique
+            // Assert : vérification que les données obtenues correspondent aux données attendues
             AChoisi? optionRecupere = context.Achoisis
                 .Where(u => u.IDPack == option.IDPack && u.IDConfigurationMoto == option.IDConfigurationMoto)
                 .FirstOrDefault();
@@ -202,25 +235,29 @@ namespace SAE_API.Controllers.Tests
             await context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Teste la méthode PostAChoisi en utilisant un mock pour simuler le référentiel de données.
+        /// Permet de vérifier le comportement du contrôleur lors de l'ajout d'un nouvel élément.
+        /// </summary>
         [TestMethod]
         public void PostAChoisiTest_Mok()
         {
-            // Arrange
+            // Arrange : préparation des données attendues
             var mockRepository = new Mock<IDataRepository<AChoisi>>();
             var userController = new AChoisiController(mockRepository.Object);
 
 
 
-            // Arrange
+            // Arrange : préparation des données attendues
             AChoisi option = new AChoisi
             {
                 IDPack = 1,
                 IDConfigurationMoto = 7,
             };
 
-            // Act
+            // Act : appel de la méthode à tester
             var actionResult = userController.PostAChoisi(option).Result;
-            // Assert
+            // Assert : vérification que les données obtenues correspondent aux données attendues
             Assert.IsInstanceOfType(actionResult, typeof(ActionResult<AChoisi>), "Pas un ActionResult<Utilisateur>");
             Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtActionResult");
 
@@ -231,17 +268,17 @@ namespace SAE_API.Controllers.Tests
             option.IDConfigurationMoto = ((AChoisi)result.Value).IDConfigurationMoto;
             Assert.AreEqual(option, (AChoisi)result.Value, "Utilisateurs pas identiques");
         }
+        #endregion
 
-
-
+        #region Test DeleteAChoisiTest
         /// <summary>
-        /// Test Delete 
+        /// Teste la méthode DeleteAChoisi pour vérifier que la suppression d'un élément fonctionne correctement.
         /// </summary>
 
         [TestMethod()]
         public void DeleteAChoisiTest()
         {
-            // Arrange
+            // Arrange : préparation des données attendues
             AChoisi option = new AChoisi
             {
                 IDPack = 1,
@@ -250,21 +287,24 @@ namespace SAE_API.Controllers.Tests
             context.Achoisis.Add(option);
             context.SaveChanges();
 
-            // Act
+            // Act : appel de la méthode à tester
             AChoisi option1 = context.Achoisis.FirstOrDefault(u => u.IDConfigurationMoto == option.IDConfigurationMoto && u.IDPack == option.IDPack);
             _ = controller.DeleteAChoisi(option.IDPack, option.IDConfigurationMoto).Result;
 
-            // Arrange
+            // Arrange : préparation des données attendues
             AChoisi res = context.Achoisis.FirstOrDefault(u => u.IDPack == option.IDPack && u.IDConfigurationMoto == option.IDConfigurationMoto);
             Assert.IsNull(res, "utilisateur non supprimé");
         }
 
-
+        /// <summary>
+        /// Teste la méthode DeleteAChoisi en utilisant un mock pour simuler le référentiel de données.
+        /// Permet de vérifier le comportement du contrôleur lors de la suppression d'un élément.
+        /// </summary>
         [TestMethod]
         public void DeleteAChoisiTest_AvecMoq()
         {
 
-            // Arrange
+            // Arrange : préparation des données attendues
             AChoisi option = new AChoisi
             {
                 IDPack = 1,
@@ -273,11 +313,12 @@ namespace SAE_API.Controllers.Tests
             var mockRepository = new Mock<IDataRepository<AChoisi>>();
             mockRepository.Setup(x => x.GetByIdAsync(option.IDPack, option.IDConfigurationMoto).Result).Returns(option);
             var userController = new AChoisiController(mockRepository.Object);
-            // Act
+            // Act : appel de la méthode à tester
             var actionResult = userController.DeleteAChoisi(option.IDPack, option.IDConfigurationMoto).Result;
-            // Assert
+            // Assert : vérification que les données obtenues correspondent aux données attendues
             Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
         }
+        #endregion
     }
-    
+
 }
