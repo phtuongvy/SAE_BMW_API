489e5193b36c766728890fb838ef88d8e5fba4a5
Shiro1234orihS <96548862+Shiro1234orihS@users.noreply.github.com>
2024-04-07 23:33:21 +0200

    j'ai fais des test pour la classe concessionnaire , couleur , datelivraison

---
+++ SAE_APITests/Controllers/CouleurEquipementsControllerTests.cs
@@ -0,0 +1,303 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SAE_API.Controllers;
+using SAE_API.Models.DataManager;
+using SAE_API.Models.EntityFramework;
+using SAE_API.Repository;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_API.Controllers.Tests
+{
+    [TestClass()]
+    public class CouleurEquipementsControllerTests
+    {
+        #region Private Fields
+        // Déclaration des variables nécessaires pour les tests
+        private CouleurEquipementsController controller;
+        private BMWDBContext context;
+        private IDataRepository<CouleurEquipement> dataRepository;
+        #endregion
+
+        #region Test Initialization
+
+        /// <summary>
+        /// Méthode d'initialisation exécutée avant chaque test pour configurer l'environnement de test.
+        /// Initialise le contexte de base de données, le référentiel de données et le contrôleur CouleurEquipement.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            // Configuration de la base de données pour les tests avec DbContextOptionsBuilder
+            var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server = 51.83.36.122; port = 5432; Database = sa25; uid = sa25; password = 1G1Nxb; SearchPath = bmw");
+            context = new BMWDBContext(builder.Options);
+            // Création du gestionnaire de données et du contrôleur à tester
+            dataRepository = new CouleurEquipementManager(context);
+            controller = new CouleurEquipementsController(dataRepository);
+        }
+        #endregion
+
+        #region Test Controller
+        /// <summary>
+        /// Teste l'instanciation du contrôleur CouleurEquipementsController pour s'assurer qu'elle n'est pas null.
+        /// </summary>
+        [TestMethod()]
+        public void CouleurEquipementsControllerTest()
+        {
+            // Arrange : préparation des données attendues
+            var builder = new DbContextOptionsBuilder<BMWDBContext>().UseNpgsql("Server = 51.83.36.122; port = 5432; Database = sa25; uid = sa25; password = 1G1Nxb; SearchPath = bmw");
+            context = new BMWDBContext(builder.Options);
+            dataRepository = new CouleurEquipementManager(context);
+
+            // Act : appel de la méthode à tester
+            var option = new CouleurEquipementsController(dataRepository);
+
+            // Assert : vérification que les données obtenues correspondent aux données attendues : vérification que les données obtenues correspondent aux données attendues
+            Assert.IsNotNull(option, "L'instance de MaClasse ne devrait pas être null.");
+
+        }
+        #endregion
+
+        #region Test GetCouleurEquipementTest
+
+        /// <summary>
+        /// Teste la méthode GetCouleurEquipements pour vérifier qu'elle retourne la liste correcte des éléments CouleurEquipement.
+        /// </summary>
+        [TestMethod()]
+        public void GetCouleurEquipementTest()
+        {
+            // Arrange : préparation des données attendues : préparation des données attendues
+            List<CouleurEquipement> expected = context.CouleurEquipements.ToList();
+            // Act : appel de la méthode à tester 
+            var res = controller.GetCouleurEquipements().Result;
+            // Assert : vérification que les données obtenues correspondent aux données attendues 
+            Assert.AreEqual(expected, res.Value.ToList(), "Les listes ne sont pas identiques");
+        }
+        #endregion
+
+        #region Test GetCouleurEquipementByIdTest
+
+        /// <summary>
+        /// Teste la méthode GetCouleurEquipementById pour vérifier qu'elle retourne l'élément correct basé sur l'ID fourni.
+        /// </summary>
+        [TestMethod()]
+        public void GetCouleurEquipementByIdTest()
+        {
+            // Arrange : préparation des données attendues
+            CouleurEquipement expected = context.CouleurEquipements.Find(1);
+            // Act : appel de la méthode à tester
+            var res = controller.GetCouleurEquipementById(expected.IdCouleurEquipement).Result;
+            // Assert : vérification que les données obtenues correspondent aux données attendues
+            Assert.AreEqual(expected, res.Value);
+        }
+
+        /// <summary>
+        /// Teste la méthode GetCouleurEquipementById en utilisant un mock pour le référentiel de données.
+        /// Permet de tester le contrôleur de manière isolée.
+        /// </summary>
+        [TestMethod]
+        public void GetCouleurEquipementByIdTest_AvecMoq()
+        {
+            // Arrange : préparation des données attendues
+            var mockRepository = new Mock<IDataRepository<CouleurEquipement>>();
+
+            CouleurEquipement option = new CouleurEquipement
+            {
+                IdCouleurEquipement = 1,
+                NomCouleurEquipement = "test",
+            };
+
+            // Act : appel de la méthode à tester
+            mockRepository.Setup(x => x.GetByIdAsync(option.IdCouleurEquipement).Result).Returns(option);
+            var userController = new CouleurEquipementsController(mockRepository.Object);
+            var actionResult = userController.GetCouleurEquipementById(option.IdCouleurEquipement).Result;
+
+            // Assert : vérification que les données obtenues correspondent aux données attendues
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(actionResult.Value);
+            Assert.AreEqual(option, actionResult.Value as CouleurEquipement);
+        }
+        #endregion
+
+        #region Test PutCouleurEquipementTestAsync
+        /// <summary>
+        /// Teste la méthode PutCouleurEquipement pour vérifier que la mise à jour d'un élément fonctionne correctement.
+        /// </summary>
+
+        [TestMethod]
+        public async Task PutCouleurEquipements_ReturnsBadRequest_WhenIdsDoNotMatch()
+        {
+            // Arrange
+            var aPourTailles = new CouleurEquipement { IdCouleurEquipement = 1 };
+
+            // Act
+            var result = await controller.PutCouleurEquipement(3, aPourTailles);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        /// <summary>
+        /// Teste la méthode PutCouleurEquipement en utilisant un mock pour simuler le référentiel de données.
+        /// Permet de vérifier le comportement du contrôleur lors de la mise à jour d'un élément.
+        /// </summary>
+
+        [TestMethod]
+        public async Task PutCouleurEquipements_ReturnsBadRequestResult_WhenCouleurEquipementDoesNotExistAsync()
+        {
+
+            // Arrange : préparation des données attendues
+            var mockRepository = new Mock<IDataRepository<CouleurEquipement>>();
+            var _controller = new CouleurEquipementsController(mockRepository.Object);
+
+            var aPourTailles = new CouleurEquipement { IdCouleurEquipement = 1 };
+            mockRepository.Setup(x => x.GetByIdAsync(1)).ReturnsAsync((CouleurEquipement)null);
+
+            // Act
+            var result = await _controller.PutCouleurEquipement(1, aPourTailles);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
+
+        [TestMethod]
+        public async Task PutCouleurEquipements_ReturnsNotFoundResult_WhenUpdateIsSuccessful()
+        {
+            // Arrange
+            var mockRepository = new Mock<IDataRepository<CouleurEquipement>>();
+            var _controller = new CouleurEquipementsController(mockRepository.Object);
+
+            var aPourTailles = new CouleurEquipement { IdCouleurEquipement = 1 };
+            mockRepository.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(aPourTailles);
+            mockRepository.Setup(x => x.UpdateAsync(It.IsAny<CouleurEquipement>(), It.IsAny<CouleurEquipement>())).Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.PutCouleurEquipement(1, aPourTailles);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
+        #endregion
+
+        #region Test PostCouleurEquipementTestAsync
+
+        /// <summary>
+        /// Teste la méthode PostCouleurEquipement pour vérifier que l'ajout d'un nouvel élément fonctionne correctement.
+        /// </summary>
+
+        [TestMethod()]
+        public async Task PostCouleurEquipementTestAsync()
+        {
+            // Arrange : préparation des données attendues
+            CouleurEquipement option = new CouleurEquipement
+            {
+                IdCouleurEquipement = 100,
+                NomCouleurEquipement = "test",
+            };
+
+            // Act : appel de la méthode à tester
+            var result = controller.PostCouleurEquipement(option).Result; // .Result pour appeler la méthode async de manière synchrone, afin d'attendre l’ajout
+
+            // Assert : vérification que les données obtenues correspondent aux données attendues
+            CouleurEquipement? optionRecupere = context.CouleurEquipements
+                .Where(u => u.IdCouleurEquipement == option.IdCouleurEquipement)
+                .FirstOrDefault();
+
+            // On ne connait pas l'ID de l’utilisateur envoyé car numéro automatique.
+            // Du coup, on récupère l'ID de celui récupéré et on compare ensuite les 2 users
+            option.IdCouleurEquipement = optionRecupere.IdCouleurEquipement;
+            Assert.AreEqual(optionRecupere, option, "Utilisateurs pas identiques");
+
+            context.CouleurEquipements.Remove(option);
+            await context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Teste la méthode PostCouleurEquipement en utilisant un mock pour simuler le référentiel de données.
+        /// Permet de vérifier le comportement du contrôleur lors de l'ajout d'un nouvel élément.
+        /// </summary>
+        [TestMethod]
+        public void PostCouleurEquipementTest_Mok()
+        {
+            // Arrange : préparation des données attendues
+            var mockRepository = new Mock<IDataRepository<CouleurEquipement>>();
+            var userController = new CouleurEquipementsController(mockRepository.Object);
+
+
+
+            // Arrange : préparation des données attendues
+            CouleurEquipement option = new CouleurEquipement
+            {
+                IdCouleurEquipement = 1,
+                NomCouleurEquipement = "test",
+            };
+
+            // Act : appel de la méthode à tester
+            var actionResult = userController.PostCouleurEquipement(option).Result;
+            // Assert : vérification que les données obtenues correspondent aux données attendues
+            Assert.IsInstanceOfType(actionResult, typeof(ActionResult<CouleurEquipement>), "Pas un ActionResult<Utilisateur>");
+            Assert.IsInstanceOfType(actionResult.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtActionResult");
+
+            var result = actionResult.Result as CreatedAtActionResult;
+            Assert.IsInstanceOfType(result.Value, typeof(CouleurEquipement), "Pas un Utilisateur");
+
+            option.IdCouleurEquipement = ((CouleurEquipement)result.Value).IdCouleurEquipement;
+            Assert.AreEqual(option, (CouleurEquipement)result.Value, "Utilisateurs pas identiques");
+        }
+        #endregion
+
+        #region Test DeleteCouleurEquipementTest
+        /// <summary>
+        /// Teste la méthode DeleteCouleurEquipement pour vérifier que la suppression d'un élément fonctionne correctement.
+        /// </summary>
+
+        [TestMethod()]
+        public void DeleteCouleurEquipementTest()
+        {
+            // Arrange : préparation des données attendues
+            CouleurEquipement option = new CouleurEquipement
+            {
+                IdCouleurEquipement = 100,
+                NomCouleurEquipement = "test",
+            };
+            context.CouleurEquipements.Add(option);
+            context.SaveChanges();
+
+            // Act : appel de la méthode à tester
+            CouleurEquipement option1 = context.CouleurEquipements.FirstOrDefault(u => u.IdCouleurEquipement == option.IdCouleurEquipement);
+            _ = controller.DeleteCouleurEquipement(option.IdCouleurEquipement).Result;
+
+            // Arrange : préparation des données attendues
+            CouleurEquipement res = context.CouleurEquipements.FirstOrDefault(u => u.IdCouleurEquipement == option.IdCouleurEquipement);
+            Assert.IsNull(res, "utilisateur non supprimé");
+        }
+
+        /// <summary>
+        /// Teste la méthode DeleteCouleurEquipement en utilisant un mock pour simuler le référentiel de données.
+        /// Permet de vérifier le comportement du contrôleur lors de la suppression d'un élément.
+        /// </summary>
+        [TestMethod]
+        public void DeleteCouleurEquipementTest_AvecMoq()
+        {
+
+            // Arrange : préparation des données attendues
+            CouleurEquipement option = new CouleurEquipement
+            {
+                IdCouleurEquipement = 1,
+                NomCouleurEquipement = "test",
+            };
+            var mockRepository = new Mock<IDataRepository<CouleurEquipement>>();
+            mockRepository.Setup(x => x.GetByIdAsync(option.IdCouleurEquipement).Result).Returns(option);
+            var userController = new CouleurEquipementsController(mockRepository.Object);
+            // Act : appel de la méthode à tester
+            var actionResult = userController.DeleteCouleurEquipement(option.IdCouleurEquipement).Result;
+            // Assert : vérification que les données obtenues correspondent aux données attendues
+            Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
+        }
+        #endregion
+    }
+}
