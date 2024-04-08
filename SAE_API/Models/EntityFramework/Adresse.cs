fab174ed1beaf5fec9abc14d89e518113b4dfa65
Shiro1234orihS <96548862+Shiro1234orihS@users.noreply.github.com>
2024-04-08 23:19:25 +0200

    j'ai ajouter RegularExpression

--- SAE_API/Models/EntityFramework/Adresse.cs
+++ SAE_API/Models/EntityFramework/Adresse.cs
@@ -22,22 +22,27 @@ namespace SAE_API.Models.EntityFramework
 
         [Column("ads_rueclient")]
         [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-,'.]{1,50}$", ErrorMessage = "La rue client n'est pas valide.")]
         public string? RueClient { get; set; }
 
         [Column("ads_codepostal")]
         [StringLength(20)]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-]{1,20}$", ErrorMessage = "Le code postal n'est pas valide.")]
         public string CodePostal { get; set; }
 
         [Column("ads_ville")]
         [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-,'.]{1,100}$", ErrorMessage = "La ville n'est pas valide.")]
         public string Ville { get; set; }
 
         [Column("ads_pays")]
         [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-,'.]{1,50}$", ErrorMessage = "Le pays n'est pas valide.")]
         public string Pays { get; set; }
 
         [Column("ads_typeadresse")]
         [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s\-,'.]{1,50}$", ErrorMessage = "Le type d'adresse n'est pas valide.")]
         public string TypeAdresse { get; set; }
 
         [ForeignKey(nameof(IdConcessionnaire))]
